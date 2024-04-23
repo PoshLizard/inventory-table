f4135cdf1e8a753a6e09ae530b9ea169070af186
tkehind1 <tkehinde96@gmail.com>
2024-04-23 12:11:26 -0400

    additions to profile information page

--- src/pages/ProfileInformation.js
+++ src/pages/ProfileInformation.js
@@ -12,14 +12,38 @@ function ProfileInformation(){
         
 
     <div>
-        <div>
+        <div id='profileHeader'>
             
-            <img className='profileLogo' src={profileLogo} />
-            <div>
+            <img style={{display:'inline-block'}} className='profileLogo' src={profileLogo} />
+            <div style={{display:'inline-block'}}>
                 <h1>Tunmise Kehinde</h1>
                 <p>Role: Manager</p>
             </div>
             
+        </div>
+        
+        <div>
+            <div>
+                <label>First Name:</label>
+                <input></input>
+            </div>
+            
+            <div>
+                <label>Last Name:</label>
+                <input></input>
+            </div>
+            
+            <div>
+                <label>Email:</label>
+                <input></input>
+            </div>
+            
+            <div>
+                 <label>Reset Password:</label>
+                 <input></input>
+            </div>
+
+            <button>Save</button>
         </div>
     </div>
 </div>
