3d5c9cc186dfc1118b24f117c06135c3a1b44125
tkehind1 <tkehinde96@gmail.com>
2024-04-29 15:30:00 -0400

    progress on profile information, get is working, still working on implementing update

--- src/pages/ProfileInformation.js
+++ src/pages/ProfileInformation.js
@@ -2,13 +2,53 @@ import profileLogo from '../images/user-profile.png'
 import Header from '../components/Header';
 import SideNav from '../components/SideNav';
 import { getAuth } from 'firebase/auth';
+import axios from 'axios';
+
+
 
 function ProfileInformation(){
 
-    const auth = getAuth();
-    const user = auth.currentUser;
+    function getProfile(){
 
-    console.log(user);
+        const auth = getAuth();
+        const user = auth.currentUser.email;
+    
+        axios.get(`http://localhost:8080/api/v1/users/lookup?email=${user}`).then(function(response) {
+            console.log(response);
+            document.getElementById("profileInfoName").innerHTML = response.data.firstName + " " + response.data.lastName;
+            document.getElementById("profileInfoRole").innerHTML = "Role: " + response.data.role;
+        }).catch(function(error){
+            alert("Error Getting Profile, Please Refresh")
+            console.log(error);
+        })
+    }
+    
+    function updateProfile() {
+    
+        const auth = getAuth();
+        const user = auth.currentUser.email;
+    
+        var updatedInfo = {
+            // id: document.getElementById("profileInfoFirstName").innerHTML,
+            firstName: document.getElementById("profileInfoFirstName").innerHTML,
+            lastName: document.getElementById("profileInfoLastName").innerHTML,
+            email: document.getElementById("profileInfoEmail").innerHTML,
+            password: document.getElementById("profileInfoPassword").innerHTML,
+            // role: document.getElementById("profileInfoRole").innerHTML,
+        }
+    
+        axios.put(`http://localhost:8080/api/v1/users/lookup?email=${user}`,updatedInfo).catch(function(error) {
+            alert("Unable to Update Profile")
+            console.log(error);
+        }).then(function () {
+            getProfile();
+            alert("Profile Updated-reorder this then to not show");
+        })
+    }
+   
+    getProfile();
+    
+
     return (
 <div>
     <Header/>
@@ -17,8 +57,8 @@ function ProfileInformation(){
         <div className='profileInfo'>
             <img className='profileLogo' src={profileLogo} />
             <div>
-                <h1>Tunmise Kehinde</h1>
-                <p>Role: Manager</p>
+                <h1 id='profileInfoName'></h1>
+                <p id='profileInfoRole'>Role: </p>
             </div>
             
             <br></br>
@@ -26,24 +66,24 @@ function ProfileInformation(){
             <br></br>
             <div className='profileFields'>
                 <label>First Name:</label>
-                <input></input>
+                <input id="profileInfoFirstName"></input>
 
                 <label>Last Name:</label>
-                <input></input>
+                <input id="profileInfoLastName"></input>
 
                 <label>Id:</label>
-                <input></input>
+                <input id="profileInfoId"></input>
 
                 <label>Email:</label>
-                <input></input>
+                <input id="profileInfoEmail"></input>
 
                 <label>Password:</label>
-                <input></input>
+                <input id="profileInfoPassword"></input>
 
                 <label>Role:</label>
-                <input></input>
+                <input id="profileInfoRole"></input>
 
-                <button>Save</button>
+                <button onClick={() => updateProfile()}>Save</button>
             </div>
             
              
