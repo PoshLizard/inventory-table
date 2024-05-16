f99bdb2bc74713fdcffc849fc95688edae3fa0d2
tkehind1 <tkehinde96@gmail.com>
2024-05-16 18:00:02 -0400

    sample table fixed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import SideNav from '../components/SideNav';
 import { Line } from "react-chartjs-2";
-import {TabulatorFull as Tabulator} from 'tabulator-tables';
+import { ReactTabulator } from 'react-tabulator'
 import {
     Chart as ChartJS,
     LineElement,
@@ -53,34 +53,23 @@ const Dashboard = () => {
     //     }
     // }
 
- //sample data
- var tabledata = [
-    {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
-    {id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
-    {id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
-    {id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
-    {id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
-];
+    const columns = [
+        { title: "Name", field: "name", width: 150 },
+        { title: "Age", field: "age", hozAlign: "left", formatter: "progress" },
+        { title: "Favourite Color", field: "col" },
+        { title: "Date Of Birth", field: "dob", hozAlign: "center" },
+        { title: "Rating", field: "rating", hozAlign: "center", formatter: "star" },
+        { title: "Passed?", field: "passed", hozAlign: "center", formatter: "tickCross" }
+      ];
+      var data = [
+        {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
+        {id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
+        {id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
+        {id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
+        {id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
+      ];
+    
 
-//create Tabulator on DOM element with id "example-table"
-var table = new Tabulator("#example-table", {
-height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-data:tabledata, //assign data to table
-layout:"fitColumns", //fit columns to width of table (optional)
-columns:[ //Define Table Columns
-    {title:"Name", field:"name", width:150},
-    {title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
-    {title:"Favourite Color", field:"col"},
-    {title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
-],
-});
-
-
-//trigger an alert message when the row is clicked
-table.on("rowClick", function(e, row){ 
-alert("Row " + row.getData().id + " Clicked!!!!");
-});
-   
     return (
         <div className='dashboard'>
             <Header />
@@ -94,7 +83,11 @@ alert("Row " + row.getData().id + " Clicked!!!!");
                             <p id='suppliesDashboard'>Supplies</p>
                         </div>
 
-                        <div id="example-table"></div>
+                        <ReactTabulator
+                        data={data}
+                        columns={columns}
+                        layout={"fitData"}
+                        />
                         
                 </div>
             </div>
